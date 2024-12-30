32e7203017eb910b63387298443c243ad5d92e02
rpillaiakshay <akshayrpillai088@gmail.com>
2024-12-30 23:18:10 +0530

    Microservices-Project

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,19 @@
+variable "aws_region" {
+  description = "AWS region"
+  default     = "ap-south-1"
+}
+
 variable "vpc_cidr" {
-  default = "10.0.0.0/16"
+  description = "CIDR block for the VPC"
+  default     = "10.0.0.0/16"
 }
 
-variable "public_subnets" {
-  default = ["10.0.1.0/24", "10.0.2.0/24"]
+variable "subnet_cidr" {
+  description = "CIDR block for the subnets"
+  default     = "10.0.1.0/24"
 }
 
-variable "private_subnets" {
-  default = ["10.0.3.0/24", "10.0.4.0/24"]
+variable "cluster_name" {
+  description = "EKS cluster name"
+  default     = "microservices-cluster"
 }
