de67ae6fdb01e514a2543946ebfe7cb66aba7003
rpillaiakshay <akshayrpillai088@gmail.com>
2024-12-30 23:22:01 +0530

    Microservices-Project

---
+++ terraform/iam_roles.tf
@@ -0,0 +1,21 @@
+resource "aws_iam_role" "eks_worker_role" {
+  name = "eks_worker_role"
+  
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_worker_policy" {
+  role       = aws_iam_role.eks_worker_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
