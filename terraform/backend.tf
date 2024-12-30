0f855a624a877a616c564d839679ff35cae4e9b0
rpillaiakshay <akshayrpillai088@gmail.com>
2024-12-30 22:51:20 +0530

    Microservices-Project

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 ﻿terraform {
   backend "s3" {
-    bucket = "microservices-terraform-state"  # Update with your actual S3 bucket name
+    bucket = "rpillaiakshay"  # Update with your actual S3 bucket name
     key    = "microservices-project/terraform.tfstate"  # Path within the S3 bucket for storing state
     region = "ap-south-1"  # Region for your AWS infrastructure
   }
