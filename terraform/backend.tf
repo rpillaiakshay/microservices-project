c7314b627bc4f837fb1c1404dc248c78e1c0980b
rpillaiakshay <akshayrpillai088@gmail.com>
2024-12-31 10:58:32 +0530

    Delete terraform/backend.tf

--- terraform/backend.tf
+++
@@ -1,7 +0,0 @@
-﻿terraform {
-  backend "s3" {
-    bucket = "rpillaiakshay"  # Update with your actual S3 bucket name
-    key    = "microservices-project/terraform.tfstate"  # Path within the S3 bucket for storing state
-    region = "ap-south-1"  # Region for your AWS infrastructure
-  }
-}
